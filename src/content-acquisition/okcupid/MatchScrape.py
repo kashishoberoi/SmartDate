40f21d6ffd49fb0fc7d5dde20986f3057fa7761e
Anjana Keshav <anjana@Anjanas-MacBook-Air.local>
2019-05-31 20:56:09 +0530

    Stores extracted data as individual json files in a pre-existing directory called tmp.

--- src/content-acquisition/okcupid/MatchScrape.py
+++ src/content-acquisition/okcupid/MatchScrape.py
@@ -3,6 +3,7 @@ import time
 from selenium.webdriver.common.by import By
 from requests import get
 from bs4 import BeautifulSoup
+import json
 
 url="https://www.okcupid.com/login"
 
@@ -12,6 +13,8 @@ password = 'rInwu9-giqvaw-viqnin'
 
 driver = webdriver.Chrome("chromedriver")
 
+
+print("Logging in!")
 driver.get(url)
 User = driver.find_element_by_name('username')
 User.clear()
@@ -27,22 +30,55 @@ form = driver.find_element_by_xpath("//input[@type='submit']")
 form.submit()
 
 time.sleep(2)
-driver.get('https://www.okcupid.com/match')
+
+print("Matches please!")
+url = "https://www.okcupid.com/match" 
+driver.get(url)
 
 page = driver.page_source
 html_soup = BeautifulSoup(page, 'html.parser')
 
+print("Getting all matches...!")
 matches = html_soup.find_all('a',class_ = 'match-results-card')
-print(matches[0])
-data_dict = {}
+traverse_url = "https://www.okcupid.com"
+
 for i in range(0,len(matches)):
-    data_dict[i] = {}
-    temp = matches[i].find('span', class_ = 'userInfo-username-name').text.split(',')
-    data_dict[i]['name'] = temp[0]
-    data_dict[i]['age'] = temp[1]
-    temp = matches[i].find('span',class_='userInfo-meta-location').text
-    data_dict[i]['location'] = temp
-    # temp = matches[i].find('div', class_ = 'match-info-percentage match-info-percentage--oval').text
-    # data_dict[i]['match_per'] = temp
-    """The commented code is the buggy bit. Still working on it"""
-print(data_dict[0])
+    #empty json intialization
+    data_dict = {}
+
+    #jumping to profile and retrieving source
+    next_profile_url = traverse_url + matches[i]['href']
+    driver.get(next_profile_url)
+    print("Entered profile", i)
+    src = driver.page_source
+    soup = BeautifulSoup(src, 'html.parser')
+
+    #retrieving Name, Age, Location and Match Percentage
+    data_dict['name'] = soup.find('div', class_='userinfo2015-basics-username').text
+    data_dict['age'] = soup.find('span',class_='userinfo2015-basics-asl-age').text
+    data_dict['location'] = soup.find('span', class_ = 'userinfo2015-basics-asl-location').text
+    match_parent = soup.find('span', class_ = 'userinfo2015-basics-asl-match')
+    data_dict['match_per'] = match_parent.find('a').text
+
+    #Initialising for category, question and answers
+    category = []
+    question = []
+    answer = []
+
+    #retrieving category, question and answers
+    for cat in soup.find_all('h2', class_ = "profile-essay-category"):
+        category.append(cat.text)
+    for ques in soup.find_all('h2', class_ = "profile-essay-title"):
+        question.append(ques.text)
+    for ans in soup.find_all('p', class_ = "profile-essay-contents"):
+        answer.append(ans.text)
+    
+    #appending to json in required format
+    for j in range(0,len(category)):
+        data_dict[category[j]] = {question[j]:answer[j]}
+    
+    #Back tracking to matches
+    driver.get(url)
+    print("Exited profile",i)
+    with open('./tmp/'+data_dict['name'].strip()+str(i)+'.json','w') as fp:
+        json.dump(data_dict, fp)
